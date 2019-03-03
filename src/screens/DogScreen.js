19f2afe65ef7bdf8fad6f4d4ea017e57e470a334
holly <hollyelizabethwhite@gmail.com>
2019-03-03 13:26:32 -0700

    css added

--- src/screens/DogScreen.js
+++ src/screens/DogScreen.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Container, Row } from 'reactstrap'
 import Dog from '../components/Dog'
+import '../styles/Doggo.css'
 
 export default class DogScreen extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Doggo Props</h1>
-        <Dog />
+      <div className="doggo-screen">
+        <Container>
+          <h1>Doggo Props</h1>
+          <Row className="doggo-row">
+            <Dog />
+          </Row>
+        </Container>
       </div>
     )
   }
